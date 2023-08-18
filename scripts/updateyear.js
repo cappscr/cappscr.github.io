92045c07cde2b5e559d4e16fa87953aa5431afd2
cappscr <capps.christopher@gmail.com>
2023-08-17 23:23:32 -0400

    Includes for core bootstrap files

--- scripts/updateyear.js
+++
@@ -1,5 +0,0 @@
-const copyrightYear = document.getElementById('copyright-year');
-const checkYear = new RegExp(/20\d\d/);
-const timeNow = new Date();
-
-copyrightYear.innerText = copyrightYear.innerText.replace(checkYear, timeNow.getFullYear());
